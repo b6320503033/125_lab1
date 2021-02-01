9fd8e55641469321742afc70fe403fda3a717717
b6320503033 <thida.sa@ku.th>
2021-02-01 19:00:55 +0700

    Update lab1_4.c

--- lab1_4.c
+++ lab1_4.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
-    int X1,X2,S;
-    scanf("%d %d",&X1,&S);
+    long int X1,X2,S;
+    scanf("%ld %ld",&X1,&S);
     X2 = (2*S)-X1;
-    printf("%d",X2);
+    printf("%ld",X2);
+    return 0;
 }
